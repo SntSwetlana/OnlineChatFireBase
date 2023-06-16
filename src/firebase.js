e8d3dc71e26f41d486030351fe8f58dc7a53cf0e
SntSwetlana <snt.swetlana@gmail.com>
2023-06-16 16:13:03 +0300

    env

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC_irvTVHuaDfvkc_KKZ7VaGJbwgFZ08X8",
-    authDomain: "sntchatterox.firebaseapp.com",
-    projectId: "sntchatterox",
-    storageBucket: "sntchatterox.appspot.com",
-    messagingSenderId: "750814597864",
-    appId: "1:750814597864:web:4195fd92d7322510db2f5d",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
     measurementId: "G-6LQV9DRETK"
   };
   
